f312523ecf440e68c22817b35b94304597e4f7f2
William D. Jones <thor0505@comcast.net>
2017-10-11 12:23:59 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+# To use a consistent encoding
+
+
+setup(
+    name="miform",
+    version=0.1,
+    description="Formal Verification Routines for Migen",
+    author="William D. Jones",
+    author_email="thor0505@comcast.net",
+    license="BSD",
+    packages=["miform"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        ],
+)
